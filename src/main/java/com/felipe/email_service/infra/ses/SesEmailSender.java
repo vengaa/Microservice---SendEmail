990e062a4a9f680d744f270da2525500512af14e
Luis <luisfelipeadm.ps@gmail.com>
2024-12-14 11:58:15 -0300

    feat: create config SesEmailSender

---
+++ src/main/java/com/felipe/email_service/infra/ses/SesEmailSender.java
@@ -0,0 +1,42 @@
+package com.felipe.email_service.infra.ses;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import com.felipe.email_service.adapters.EmailSenderGateway;
+import com.felipe.email_service.core.exceptions.EmailServiceException;
+
+@Service
+public class SesEmailSender implements EmailSenderGateway {
+	
+	private final AmazonSimpleEmailService amazonSimpleEmailService;
+	
+	@Autowired
+	public  SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService) {
+		this.amazonSimpleEmailService = amazonSimpleEmailService;
+	}
+
+	@Override
+	public void sendEmail(String to, String subject, String body) {
+		SendEmailRequest request = new SendEmailRequest()
+				.withSource("luisfelipeadm.wsp@gmail.com")
+				.withDestination(new Destination().withToAddresses(to))
+				.withMessage(new Message().withSubject(new Content(subject)).withBody(new Body().withText(new Content(body)).withHtml(new Content(body))));
+		
+		try {
+			this.amazonSimpleEmailService.sendEmail(request);
+		} catch (AmazonServiceException e) {
+			throw new EmailServiceException("Failure wile sending email", e);
+		}
+		
+	}
+	
+
+}
